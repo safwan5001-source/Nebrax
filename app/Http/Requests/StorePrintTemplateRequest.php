8a72c596f5de1759035f51bd8b362223936156b4
safwan5001-source <safwan5001@gmail.com>
2026-08-26 21:57:06 +0300

    feat(documents): validate language mode when creating templates

--- app/Http/Requests/StorePrintTemplateRequest.php
+++ app/Http/Requests/StorePrintTemplateRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use App\Support\DocumentLanguageMode;
 use App\Support\PrintTemplateContract;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
@@ -20,6 +21,7 @@ class StorePrintTemplateRequest extends FormRequest
             'document_types'           => ['required', 'array', 'min:1', 'max:13'],
             'document_types.*'         => ['required', 'string', Rule::in(PrintTemplateContract::DOCUMENT_TYPES)],
             'definition'               => ['required', 'array', 'max:100'],
+            'definition.language_mode' => ['sometimes', 'string', Rule::in(DocumentLanguageMode::VALUES)],
             'schema_version'           => ['sometimes', 'integer', 'min:1', 'max:32767'],
         ];
     }
